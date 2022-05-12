5ab4524fc7fb5126055273acf0e66184b971eefe
mustafahaneci <93029887+mustafahaneci@users.noreply.github.com>
2022-05-13 02:28:27 +0300

    commit

--- routes/Articles.js
+++ routes/Articles.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Article = require("../models/article")
 
 router.get("/new", (req, res) =>{
+    res.render("articles/new", {article: new Article()})
 })
 
 module.exports = router

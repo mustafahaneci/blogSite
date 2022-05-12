c2bf164c75f1f9bb3dd63ea0b20805d23a85d987
mustafahaneci <93029887+mustafahaneci@users.noreply.github.com>
2022-05-13 02:19:47 +0300

    commit

--- models/article.js
+++ models/article.js
@@ -1,1 +1,7 @@
 const mongoose = require("mongoose")
+
+const articleSchema = new mongoose.Schema({
+    title: {type: String, require: true},
+    content: {type: String, require: true},
+    createdAt: {type: Date, default: Date.now}
+})
